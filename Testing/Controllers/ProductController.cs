c118d0211a9d73afb0240d54b529f7e1b9742dc9
Riley Plokhooy <rileyanthony2006@gmail.com>
2026-03-30 19:15:42 -0400

    done

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }            
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
